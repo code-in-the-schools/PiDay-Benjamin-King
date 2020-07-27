da018fdbd3be94ece7a55a5c05313701e30455e1
BenjaminKing39 <bekingly39@gmail.com>
2020-07-27 15:56:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#New Function
+#def my_function(x,y,z):
+  #print("Hello from a function")
+
+
+PiDay = 'March, 14, 2021'
+def Bday_Function(Month,Day,Year,Name):
+ print(Month = input('Enter the month you were born'))
+ print(Day = int(input('Enter the day you were born')))
+ print(Year = int(input('Enter the year you were born')))
+ print(Name = input('Enter Your Name'))
+
+print(Bday_Function
+
+
